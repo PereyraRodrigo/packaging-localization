32e69a5d8421d3730394cd7d4800400149e6e364
Rodrigo Pereyra <pereyrarodrigo@hotmail.com>
2023-01-20 21:00:23 -0300

    9th commit

--- src/app/pages/home/components/our-work/our-work.component.ts
+++ src/app/pages/home/components/our-work/our-work.component.ts
@@ -16,27 +16,32 @@ export class OurWorkComponent implements OnInit {
     {
       "img": "../../../../../assets/img/portfolio-items/Slime-Rancher-Portfolio.jpg",
       "title": "Slime Rancher",
-      "company": "Packaging"
+      "company": "Packaging",
+      "link":"https://www.waypoint.la/portfolio/creative/slime-rancher-packaging-design"
     },
     {
       "img": "../../../../../assets/img/portfolio-items/goose.png",
       "title": "Untitled Goose Game",
-      "company": "Packaging and Localization"
+      "company": "Packaging and Localization",
+      "link":"https://www.waypoint.la/portfolio/creative/untitled-goose-game-packaging-design-and-localization"
     },
     {
       "img": "../../../../../assets/img/portfolio-items/Deaths-Gambit-Packaging_Media_Image05-V2.jpg",
       "title": "Death's Gambit",
-      "company": "Packaging Art"
+      "company": "Packaging Art",
+      "link":"https://www.waypoint.la/portfolio/creative/deaths-gambit-ps4-retail-box-art"
     },
     {
       "img": "../../../../../assets/img/portfolio-items/disco-elysium.png",
       "title": "Disco Elysium",
-      "company": "Localization"
+      "company": "Localization",
+      "link":"https://www.waypoint.la/portfolio/creative/disco-elysium-packaging-and-loc"
     },
     {
       "img": "../../../../../assets/img/portfolio-items/dnd.png",
       "title": "D&D",
-      "company": "Key Art and Packaging"
+      "company": "Key Art and Packaging",
+      "link":"https://www.waypoint.la/portfolio/creative/dd-key-art-and-packaging"
     }
   ]
   constructor() { }
