625c090d838219ddb2239383ca8480ac14ad748c
Rodrigo Pereyra <pereyrarodrigo@hotmail.com>
2023-01-23 18:35:10 -0300

    haha2

--- src/app/pages/home/components/slider/slider.component.ts
+++ src/app/pages/home/components/slider/slider.component.ts
@@ -25,15 +25,12 @@ export class SliderComponent implements OnInit {
   slideConfig = {
     slidesToShow: 3,
     slidesToScroll: 1,
-    dots:true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-          slidesToScroll:1,
-          infinite: true,
-          dots: true
+          slidesToScroll:1
         }
       },
       {
